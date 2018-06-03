eb833a14b9e036c1a20481ea097db9f54129693e
TweakoZ <michael@tweakoz.com>
2018-06-02 23:52:08 -0700

    nexys4 (non-ddr) platform/target (WIP)

---
+++ litex/boards/platforms/nexys4.py
@@ -0,0 +1,81 @@
+# This file is Copyright (c) 2018 Florent Kermarrec <florent@enjoy-digital.fr>
+# License: BSD
+
+from litex.build.generic_platform import *
+from litex.build.xilinx import XilinxPlatform, XC3SProg, VivadoProgrammer
+
+_io = [
+    ("user_led",  0, Pins("H17"), IOStandard("LVCMOS33")),
+    ("user_led",  1, Pins("K15"), IOStandard("LVCMOS33")),
+    ("user_led",  2, Pins("J13"), IOStandard("LVCMOS33")),
+    ("user_led",  3, Pins("N14"), IOStandard("LVCMOS33")),
+    ("user_led",  4, Pins("R18"), IOStandard("LVCMOS33")),
+    ("user_led",  5, Pins("V17"), IOStandard("LVCMOS33")),
+    ("user_led",  6, Pins("U17"), IOStandard("LVCMOS33")),
+    ("user_led",  7, Pins("U16"), IOStandard("LVCMOS33")),
+    ("user_led",  8, Pins("V16"), IOStandard("LVCMOS33")),
+    ("user_led",  9, Pins("T15"), IOStandard("LVCMOS33")),
+    ("user_led", 10, Pins("U14"), IOStandard("LVCMOS33")),
+    ("user_led", 11, Pins("T16"), IOStandard("LVCMOS33")),
+    ("user_led", 12, Pins("V15"), IOStandard("LVCMOS33")),
+    ("user_led", 13, Pins("V14"), IOStandard("LVCMOS33")),
+    ("user_led", 14, Pins("V12"), IOStandard("LVCMOS33")),
+    ("user_led", 15, Pins("V11"), IOStandard("LVCMOS33")),
+
+    ("user_sw",  0, Pins("J15"), IOStandard("LVCMOS33")),
+    ("user_sw",  1, Pins("L16"), IOStandard("LVCMOS33")),
+    ("user_sw",  2, Pins("M13"), IOStandard("LVCMOS33")),
+    ("user_sw",  3, Pins("R15"), IOStandard("LVCMOS33")),
+    ("user_sw",  4, Pins("R17"), IOStandard("LVCMOS33")),
+    ("user_sw",  5, Pins("T18"), IOStandard("LVCMOS33")),
+    ("user_sw",  6, Pins("U18"), IOStandard("LVCMOS33")),
+    ("user_sw",  7, Pins("R13"), IOStandard("LVCMOS33")),
+    ("user_sw",  8, Pins("T8"), IOStandard("LVCMOS33")),
+    ("user_sw",  9, Pins("U8"), IOStandard("LVCMOS33")),
+    ("user_sw", 10, Pins("R16"), IOStandard("LVCMOS33")),
+    ("user_sw", 11, Pins("T13"), IOStandard("LVCMOS33")),
+    ("user_sw", 12, Pins("H6"), IOStandard("LVCMOS33")),
+    ("user_sw", 13, Pins("U12"), IOStandard("LVCMOS33")),
+    ("user_sw", 14, Pins("U11"), IOStandard("LVCMOS33")),
+    ("user_sw", 15, Pins("V10"), IOStandard("LVCMOS33")),
+
+    ("user_btn", 0, Pins("N17"), IOStandard("LVCMOS33")),
+    ("user_btn", 1, Pins("P18"), IOStandard("LVCMOS33")),
+    ("user_btn", 2, Pins("P17"), IOStandard("LVCMOS33")),
+    ("user_btn", 3, Pins("M17"), IOStandard("LVCMOS33")),
+    ("user_btn", 4, Pins("M18"), IOStandard("LVCMOS33")),
+
+    ("clk100", 0, Pins("E3"), IOStandard("LVCMOS33")),
+
+    ("cpu_reset", 0, Pins("C12"), IOStandard("LVCMOS33")),
+
+    ("serial", 0,
+        Subsignal("tx", Pins("D4")),
+        Subsignal("rx", Pins("C4")),
+        IOStandard("LVCMOS33"),
+    ),
+
+]
+
+
+class Platform(XilinxPlatform):
+    default_clk_name = "clk100"
+    default_clk_period = 10.0
+
+    def __init__(self, programmer="vivado"):
+        XilinxPlatform.__init__(self, "xc7a100t-CSG324-1", _io, toolchain="vivado")
+        self.programmer = programmer
+        self.add_platform_command("set_property INTERNAL_VREF 0.750 [get_iobanks 34]")
+
+
+    def create_programmer(self):
+        if self.programmer == "xc3sprog":
+            return XC3SProg("nexys4")
+        elif self.programmer == "vivado":
+            return VivadoProgrammer()
+        else:
+            raise ValueError("{} programmer is not supported"
+                             .format(self.programmer))
+
+    def do_finalize(self, fragment):
+        XilinxPlatform.do_finalize(self, fragment)
